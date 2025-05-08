394d5652ec16d97ee690a98f689e94285dfe9e9e
gitmskhl <gglmskhl@gmail.com>
2025-05-08 23:28:09 +0300

    it can be clicked but not dragged

--- player_window.py
+++ player_window.py
@@ -6,32 +6,38 @@ from PySide6 import QtMultimedia, QtCore
 class PlayerWindow(QDialog):
     def __init__(self):
         super().__init__()
-        self.saved_volume = 50
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
         self.show()
-        self.saved_volume = 50
+
+        self.saved_volume = 50 
         self.ui.pushButton_4.clicked.connect(self.addFiles)
+        
         self.player = QtMultimedia.QMediaPlayer()
         self.audiooutput = QtMultimedia.QAudioOutput()
         self.audiooutput.setVolume(1)
+        self.player.setAudioOutput(self.audiooutput)
+
         self.ui.verticalSlider.setValue(100)
         self.ui.verticalSlider.sliderMoved.connect(self.volume)
         self.ui.verticalSlider.sliderMoved.connect(self.volume_off)
         self.ui.pushButton_8.clicked.connect(self.volume_off)
+        
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.moveSlider)
-        self.player.setAudioOutput(self.audiooutput)
+        
         self.ui.pushButton_7.clicked.connect(self.play)
         self.ui.horizontalSlider.sliderMoved.connect(self.musicMoved)
+
         self.fileNames = []
         self.ui.horizontalSlider.mousePressEvent = self.mousePressEvent
+
     def addFiles(self):
         fileNames = QFileDialog.getOpenFileNames(None, 'Выберите файл', '', 'Audio Files (*.mp3 *.wav *.ogg)')
         for i in fileNames[0]:
-            print(i)
             self.ui.listWidget.addItem(os.path.basename(i))
             self.fileNames.append(i)
+
 
     def play(self, is_pause: True):
         if is_pause == True:
@@ -50,10 +56,12 @@ class PlayerWindow(QDialog):
             self.timer.stop
             self.player.setPosition(self.player.position()) 
     
+
     def volume(self, newValue=None):
         if newValue is not None:
             self.audiooutput.setVolume(newValue/100)
         return self.audiooutput.volume() * 100
+
 
     def volume_off(self, is_off=False):
         if is_off:
@@ -62,22 +70,27 @@ class PlayerWindow(QDialog):
         else:
             self.volume(self.saved_volume)
     
+
     def moveSlider(self):
         self.ui.horizontalSlider.setValue(self.player.position())
+
+
     def musicMoved(self, pos):
         self.player.setPosition(pos)
+    
+    
     def mousePressEvent(self, event):
-        value = self.ui.horizontalSlider.minimum() + ((self.ui.horizontalSlider.maximum() - self.ui.horizontalSlider.minimum()) * event.position().x()) / self.ui.horizontalSlider.width()
-        self.ui.horizontalSlider.setValue(int(value))
-        self.player.setPosition(int(value))
-        event.accept()
-        global_pos = self.ui.horizontalSlider.mapToGlobal(self.ui.horizontalSlider.pos())
-        print(global_pos)
-        print(event.position().x())
-        
+        if event.button() == QtCore.Qt.MouseButton.LeftButton:
+            click_global_pos = event.globalPosition().toPoint()
+            slider_pos = self.ui.horizontalSlider.mapFromGlobal(click_global_pos)
+            click_relative_x = slider_pos.x()
 
+            slider = self.ui.horizontalSlider
+            slider_range = slider.maximum() - slider.minimum()
+            slider_value = int((click_relative_x / slider.width()) * slider_range) + slider.minimum()
+            slider.setValue(slider_value)
+            self.player.setPosition(slider_value)
 
+            self.ui.horizontalSlider.setSliderDown(True)
 
-        
-
-        
+        super(QSlider, self.ui.horizontalSlider).mousePressEvent(event)
