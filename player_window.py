b3efca046013fd64e7bac0a85cca560493ec2d43
BABUIN04 <ivan.shinkarenko.99@mail.ru>
2025-05-09 19:42:18 +0300

    normal version

--- player_window.py
+++ player_window.py
@@ -3,48 +3,41 @@ from interface_ui import Ui_Dialog
 import os
 from PySide6 import QtMultimedia, QtCore
 
-
 class PlayerWindow(QDialog):
     def __init__(self):
         super().__init__()
+        self.saved_volume = 50
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
         self.show()
-
-        self.saved_volume = 50 
+        self.saved_volume = 50
         self.ui.pushButton_4.clicked.connect(self.addFiles)
-        
         self.player = QtMultimedia.QMediaPlayer()
         self.audiooutput = QtMultimedia.QAudioOutput()
         self.audiooutput.setVolume(1)
-        self.player.setAudioOutput(self.audiooutput)
-
         self.ui.verticalSlider.setValue(100)
         self.ui.verticalSlider.sliderMoved.connect(self.volume)
-        self.ui.verticalSlider.sliderMoved.connect(self.volume_off)
-        self.ui.pushButton_8.clicked.connect(self.volume_off)
-        
+        #self.ui.verticalSlider.sliderMoved.connect(self.volume_off)
+        #self.ui.pushButton_8.clicked.connect(self.volume_off)
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.moveSlider)
-        
+        self.player.setAudioOutput(self.audiooutput)
         self.ui.pushButton_7.clicked.connect(self.play)
         self.ui.horizontalSlider.sliderMoved.connect(self.musicMoved)
-
         self.fileNames = []
-
+        self.ui.horizontalSlider.mousePressEvent = self.mousePressEvent
     def addFiles(self):
         fileNames = QFileDialog.getOpenFileNames(None, 'Выберите файл', '', 'Audio Files (*.mp3 *.wav *.ogg)')
         for i in fileNames[0]:
+            print(i)
             self.ui.listWidget.addItem(os.path.basename(i))
             self.fileNames.append(i)
 
-
-    def play(self, is_pause = True):
+    def play(self, is_pause: True):
         if is_pause == True:
-            # не оч понял, зачем еще раз проверять одно и тоеж
             if self.player.playbackState() == QtMultimedia.QMediaPlayer.PausedState:
                 self.player.play()
-            else: # тут же наоборот чекается, что музыка играла, значит ее надо остановить, а ниже она включается
+            else:
                 idx = self.ui.listWidget.currentRow()
                 fileName = self.fileNames[idx]
                 self.player.setSource(QtCore.QUrl.fromLocalFile(fileName))
@@ -54,28 +47,37 @@ class PlayerWindow(QDialog):
                 self.timer.start(10)
         else:
             self.player.pause()
-            self.timer.stop() # это вроде функция, тут было self.timer.stop
-            self.player.setPosition(self.player.position())  # зачем?
+            self.timer.stop
+            self.player.setPosition(self.player.position()) 
     
-
     def volume(self, newValue=None):
-        if newValue is not None:
-            self.audiooutput.setVolume(newValue/100)
-        return self.audiooutput.volume() * 100
+        #if newValue is not None:
+        self.audiooutput.setVolume(newValue/100)
+        #return self.audiooutput.volume() * 100
 
-
-    def volume_off(self, is_off=False):
+    '''def volume_off(self, is_off=False):
         if is_off:
             self.saved_volume = self.volume()  
             self.volume(0) 
         else:
-            self.volume(self.saved_volume)
+            self.volume(self.saved_volume)'''
     
-
     def moveSlider(self):
         self.ui.horizontalSlider.setValue(self.player.position())
-
-
     def musicMoved(self, pos):
         self.player.setPosition(pos)
-    
+    def mousePressEvent(self, event):
+        value = self.ui.horizontalSlider.minimum() + ((self.ui.horizontalSlider.maximum() - self.ui.horizontalSlider.minimum()) * event.position().x()) / self.ui.horizontalSlider.width()
+        self.ui.horizontalSlider.setValue(int(value))
+        self.player.setPosition(int(value))
+        event.accept()
+        global_pos = self.ui.horizontalSlider.mapToGlobal(self.ui.horizontalSlider.pos())
+        print(global_pos)
+        print(event.position().x())
+        
+
+
+
+        
+
+        
